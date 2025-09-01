71b4f46bad4b2a869786dcbc8c928627572abc67
Ishan Shahnan <162598177+Ishan-shahnan@users.noreply.github.com>
2025-09-01 20:31:07 +0600

    Update Static

--- Vaccination_Management_System/urls.py
+++ Vaccination_Management_System/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -23,7 +25,10 @@ urlpatterns = [
     path("api/", include("api.urls")),
     path("api/auth/", include("users.urls")),
     # Swagger UI endpoints
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/redoc/', SpectacularSwaggerView.as_view(url_name='schema'), name='redoc'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularSwaggerView.as_view(url_name='schema'), name='redoc'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
