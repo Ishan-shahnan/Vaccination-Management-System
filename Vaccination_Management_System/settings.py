b43b76b07938ae674d923b49c6622e1d98a2c2af
Ishan Shahnan <162598177+Ishan-shahnan@users.noreply.github.com>
2025-10-02 14:01:11 +0600

    Update settings.py

--- Vaccination_Management_System/settings.py
+++ Vaccination_Management_System/settings.py
@@ -30,6 +30,8 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 # Application definition
 
 INSTALLED_APPS = [
